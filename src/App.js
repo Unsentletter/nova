0c2d0344c9640df304404ca82a6e71788c95a568
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-09 10:14:31 +1100

    InputChange and FetchWeath functions added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import 'materialize-css/dist/css/materialize.min.css';
+
 import './App.css';
 
+
+import { SearchBar } from './components/SearchBar';
+
+const apiKey = 'fb1a712a82a2133d66d27b355139f842';
+const tempLocation = '37.8267,-122.4233';
+const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${apiKey}/${tempLocation}`;
+
 class App extends Component {
+  constructor(props) {
+    super(props, null, null);
+    this.state = {
+      term: ''
+    }
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.fetchWeatherData(this.state.term);
+    this.setState({ term: '' })
+  };
+
+  fetchWeatherData = (term) => {
+    axios.get(url).then(res => {
+      console.log(res);
+    }, (err) => {
+      console.log(err)
+    })
+  };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value })
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SearchBar onFormSubmit={this.onFormSubmit} onInputChange={this.onInputChange}/>
       </div>
     );
   }

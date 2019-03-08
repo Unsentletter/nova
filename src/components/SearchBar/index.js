2b7040057256a5d543d77a827555a1e3ebdf65b2
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-09 10:13:54 +1100

    SearchBar functional component created

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from '../Button';
+
+export const SearchBar = props => {
+  console.log(props);
+  return (
+    <form onSubmit={props.onFormSubmit} className="input-group">
+      <input onChange={props.onInputChange}/>
+      <Button label="Search"/>
+    </form>
+  )
+};

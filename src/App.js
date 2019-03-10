69cc1f0108daa99bdd949d8f3d7cc3fc6de6bd80
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-10 16:54:00 +1100

    Components added to app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import 'materialize-css/dist/css/materialize.min.css';
 
 import './App.css';
 
-
 import { SearchBar } from './components/SearchBar';
+import { MainDisplay } from './components/MainDisplay';
+import { SubDisplay } from "./components/SubDisplay";
 
 const apiKey = 'fb1a712a82a2133d66d27b355139f842';
 const tempLocation = '37.8267,-122.4233';
@@ -15,33 +16,57 @@ class App extends Component {
   constructor(props) {
     super(props, null, null);
     this.state = {
-      term: ''
+      term: '',
+      weather: {}
     }
   }
 
   onFormSubmit = (event) => {
     event.preventDefault();
 
+    this.setState({ term: '' });
     this.fetchWeatherData(this.state.term);
-    this.setState({ term: '' })
   };
 
   fetchWeatherData = (term) => {
     axios.get(url).then(res => {
-      console.log(res);
+      console.log("APP", res);
+      this.setState({
+        weather: res.data.hourly
+      })
     }, (err) => {
       console.log(err)
     })
   };
+
+  orderDays = () => {
+    const dayOfWeek = new Date().getDay();
+    const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let reorderedWeekdays = [];
+
+    for(let i = 0; i < weekdays.length; i++) {
+      let pointer = (i + dayOfWeek) % weekdays.length;
+      reorderedWeekdays.push(weekdays[pointer])
+    }
+
+
+
+    return reorderedWeekdays
+  }
 
   onInputChange = event => {
     this.setState({ term: event.target.value })
   };
 
   render() {
+    console.log(this.orderDays())
     return (
       <div>
         <SearchBar onFormSubmit={this.onFormSubmit} onInputChange={this.onInputChange}/>
+        <MainDisplay data={this.state}/>
+        {this.orderDays().map((day, i) => {
+          return <SubDisplay key={i} day={day}/>
+        })}
       </div>
     );
   }

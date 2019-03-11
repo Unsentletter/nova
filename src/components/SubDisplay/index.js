47a20fb9e5958484aa3d81abc5e2f08a41bdcdd4
Tristan Southwell <tristan.southwell@hotmail.com>
2019-03-11 21:47:08 +1100

    SubDisplay styled

--- src/components/SubDisplay/index.js
+++ src/components/SubDisplay/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import './index.css';
+
 export class SubDisplay extends Component {
 
   render() {
     return (
-      <div>{this.props.day}</div>
+      <div className='sub-display'>
+        <div className='title'>
+         {this.props.day}
+        </div>
+        <div className='icon-field'>
+          {this.props.weather.icon}
+        </div>
+      </div>
     )
   }
 }
